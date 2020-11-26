212033bba4c44cb85fac1c6f90c0c3e036065fc9
Can Gür <cangur2398@gmail.com>
2020-11-26 18:15:54 +0300

    created category card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Grid } from 'semantic-ui-react';
 import './App.css';
 import ItemCard from './components/card.js'
+import CategoryCard from './components/categoryCard.js'
 function App() {
   return (
     <div className="App">
+      <Grid columns={3}>
+        <Grid.Column>
+          <CategoryCard />
+        </Grid.Column>
+        <Grid.Column>
+          <CategoryCard />
+        </Grid.Column>
+        <Grid.Column>
+          <CategoryCard />
+        </Grid.Column>
+      </Grid>
+
       <ItemCard />
       <ItemCard />
       <ItemCard />
